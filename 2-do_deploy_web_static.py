e6127b0ea52d63ca7cbc551280c5b63080acc87f
LuisColorado-tech <luiscolorado@outlook.com>
2021-08-26 15:34:09 -0500

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,57 @@
 #!/usr/bin/python3
-"""Deploy archive"""
-from fabric.contrib import files
-from fabric.api import env, put, run
-import os
+"""doing a deploming for all the services"""
 
-env.hosts = ['34.75.8.225', '54.91.28.2']
+import os
+from re import A
+import re
+from fabric.api import *
+
+env.hosts = ["34.73.50.135", "52.73.46.71"]
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """do_deploy
-    Args:
-        archive_path
-    """
-    if not os.path.exists(archive_path):
+    """uploading a file to the remotes servers"""
+    if not os.path.isfile(archive_path):
         return False
 
-    data_path = '/data/web_static/releases/'
-    tmp = archive_path.split('.')[0]
-    name = tmp.split('/')[1]
-    dest = data_path + name
-
-    try:
-        put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(dest))
-        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
-        run('rm -f /tmp/{}.tgz'.format(name))
-        run('mv {}/web_static/* {}/'.format(dest, dest))
-        run('rm -rf {}/web_static'.format(dest))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(dest))
-        return True
-    except:
+    upload = put(archive_path, "/tmp/")
+    if upload.failed:
         return False
+
+    file_ex = archive_path.split("/")
+    file_out_ext = file_ex[1].split(".")
+
+    direct = run(
+        "mkdir -p /data/web_static/releases/{}".format(file_out_ext[0]))
+    if direct.failed:
+        return False
+
+    uncompress = run(
+        "tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+            file_ex[1], file_out_ext[0]))
+    if uncompress.failed:
+        return False
+
+    removed = run("rm /tmp/{}".format(file_ex[1]))
+    if removed.failed:
+        return False
+
+    movinf_file = run(
+        "mv /data/web_static/releases/{}/web_static/*\
+         /data/web_static/releases/{}".format(
+            file_out_ext[0], file_out_ext[0]))
+    if movinf_file.failed:
+        return False
+
+    link_rm = run("rm -rf /data/web_static/current")
+    if link_rm.failed:
+        return False
+
+    new_link = run(
+        "ln -s /data/web_static/releases/{} /data/web_static/current".format(
+            file_out_ext[0]))
+    if new_link.failed:
+        return False
+
+    return True
