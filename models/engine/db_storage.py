e4e66c81e5307fbb60790ea88631b99f4dc5ca5a
LuisColorado-tech <luiscolorado@outlook.com>
2021-09-12 19:45:13 -0500

    add files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,3 +71,7 @@ class DBStorage:
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+        
+    def close(self):
+        """removing the session"""
+        self.__session.close()
